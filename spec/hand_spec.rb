aac406a989fb792884c7e8f4421acd4a551c322a
App Academy Student <student@appacademy.io>
2015-07-03 17:10:57 -0700

    Expand hand spec to check combos

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -19,27 +19,142 @@ require 'card'
 describe Hand do
   let(:deck) { double("deck") }
   subject(:hand) { Hand.new }
-  # hand = [
-  #   Card.new(:heart, :two)
-  #   Card.new(:spade, :three)
-  #   Card.new(:heart, :five)
-  #   Card.new(:heart, :six)
-  #   Card.new(:heart, :nine)
-  #   ]
+  hand = [
+    Card.new(:heart, :two),
+    Card.new(:spade, :three),
+    Card.new(:heart, :five),
+    Card.new(:heart, :six),
+    Card.new(:heart, :nine)
+    ]
+  let(:straight_flush) { Hand.new([
+    Card.new(:heart, :two),
+    Card.new(:heart, :three),
+    Card.new(:heart, :four),
+    Card.new(:heart, :six),
+    Card.new(:heart, :five)
+    ]) }
+  let(:four_of_a_kind) { Hand.new([
+    Card.new(:heart, :three),
+    Card.new(:diamond, :three),
+    Card.new(:spade, :three),
+    Card.new(:club, :three),
+    Card.new(:heart, :five)
+    ]) }
+  let(:full_house) { Hand.new([
+    Card.new(:heart, :three),
+    Card.new(:diamond, :three),
+    Card.new(:spade, :three),
+    Card.new(:club, :five),
+    Card.new(:heart, :five)
+    ]) }
+  let(:flush) { Hand.new([
+    Card.new(:heart, :two),
+    Card.new(:heart, :three),
+    Card.new(:heart, :four),
+    Card.new(:heart, :seven),
+    Card.new(:heart, :five)
+    ]) }
+  let(:straight) { Hand.new([
+    Card.new(:heart, :two),
+    Card.new(:heart, :three),
+    Card.new(:heart, :four),
+    Card.new(:diamond, :six),
+    Card.new(:heart, :five)
+    ]) }
+  let(:three_of_a_kind) { Hand.new([
+    Card.new(:heart, :two),
+    Card.new(:spade, :two),
+    Card.new(:heart, :four),
+    Card.new(:diamond, :two),
+    Card.new(:heart, :five)
+    ]) }
+  let(:two_pair { Hand.new([
+    Card.new(:heart, :two),
+    Card.new(:diamond, :two),
+    Card.new(:club, :six),
+    Card.new(:diamond, :six),
+    Card.new(:heart, :five)
+    ]) }
+  let(:pair) { Hand.new([
+    Card.new(:heart, :two),
+    Card.new(:heart, :three),
+    Card.new(:heart, :four),
+    Card.new(:diamond, :two),
+    Card.new(:heart, :five)
+    ]) }
 
-  describe "#initialize" do
+  describe "#deal_from" do
     it "should deal five cards to the hand" do
-      #allow(deck).to receive(:deal).and_return([hand])
-      Hand.
-      expect(Hand.deal_from(deck).).
+      allow(deck).to receive(:deal(5)).and_return(hand)
+      test_hand = Hand.deal_from(deck)
+      expect(test_hand.cards.length).to eq(5)
+    end
+  end
 
+  describe "#combo?" do
+    it "returns true if there's a combo" do
+      expect(pair).to be_combo
+    end
+
+    it "doesn't return false positives" do
+      expect(hand).not_to be_combo
+    end
+  end
+
+  describe "#determine_combo_value" do
+    it "should return 8 for a straight flush" do
+      expect(straight_flush.determine_combo_value).to eq(8)
+    end
+    it "should return 7 for a four of a kind" do
+      expect(four_of_a_kind.determine_combo_value).to eq(7)
+    end
+    it "should return 6 for a full house" do
+      expect(full_house.determine_combo_value).to eq(6)
+    end
+    it "should return 5 for a flush" do
+      expect(flush.determine_combo_value).to eq(5)
+    end
+    it "should return 4 for a straight" do
+      expect(straight.determine_combo_value).to eq(4)
+    end
+    it "should return 3 for a three of a kind" do
+      expect(three_of_a_kind.determine_combo_value).to eq(3)
+    end
+    it "should return 2 for a two pair" do
+      expect(two_pair.determine_combo_value).to eq(2)
+    end
+    it "should return 1 for a pair" do
+      expect(pair.determine_combo_value).to eq(1)
+    end
+  end
+
+  describe "#best_combo" do
+    describe "should prioritize better combos" do
+      it "knows a full house is better than a 3 of a kind" do
+        expect(full_house.best_combo).to eq(:full_house)
+      end
+
+      it "knows a straight flush is better than a straight or flush" do
+        expect(straight_flush.best_combo).to eq(:straight_flush)
+      end
+
+      it "knows a 2 pair is better than a pair" do
+        expect(two_pair.best_combo).to eq (:two_pair)
+      end
     end
   end
 
 
-  def self.deal_from(deck)
-    Hand.new(deck.deal(5))
+
+
   end
+
+  describe "#value" do
+    it "should correctly return the sum of the cards" do
+      expect(hand.value).to eq(25)
+    end
+  end
+
 
 
 
